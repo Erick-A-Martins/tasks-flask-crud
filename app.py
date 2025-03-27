0ca4164ce3fd24fd0153e53dcb49711e2bc2a56e
Erick-A-Martins <erickalvesqmartins@gmail.com>
2025-03-27 15:46:09 -0300

    add delete method and done project

--- app.py
+++ app.py
@@ -44,6 +44,7 @@ def update_task(id):
     for t in tasks:
         if t.id == id:
             task = t
+            break
 
     print(task)
     if task == None:
@@ -55,6 +56,20 @@ def update_task(id):
     task.completed = data['completed']
     print(task)
     return jsonify({"message": "Tarefa atualizada com sucesso"})
+
+@app.route('/tasks/<int:id>', methods=['DELETE'])
+def delete_task(id):
+    task = None
+    for t in tasks:
+        if t.id == id:
+            task = t
+            break
+
+    if not task:
+        return jsonify({"message": "Não foi possível encontrar a atividade"}), 404
+    
+    tasks.remove(task)
+    return jsonify({"message": "Tarefa deletada com sucesso"})
 
 if __name__ == "__main__":
     app.run(debug=True)
